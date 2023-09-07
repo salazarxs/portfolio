3c94935c57f054e7cae5ab2dc516ed8434fb8ffb
salazarxs <martinpaolo10@gmail.com>
2023-09-06 21:33:33 -0300

    desktop 90% ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import './App.css';
 import NavBar from './components/NavBar';
 
 // iconst
-import { FaCalendarDays } from 'react-icons/fa6';
-import { BsTelephoneFill } from 'react-icons/bs';
+import { FaCalendarDays, FaReact } from 'react-icons/fa6';
+import { BsTelephoneFill, BsWordpress } from 'react-icons/bs';
 import { TbBrandMailgun } from 'react-icons/tb';
 import { BiSolidMap } from 'react-icons/bi';
+import { TbDevicesCode } from 'react-icons/tb';
+import { CgShapeZigzag } from 'react-icons/cg';
 
 // images
 import profileIamge from './images/profile-image.jpg';
+import Card from './components/Card';
+import imgCalculator from './images/calculator.avif';
 
 
 function App() {
@@ -28,22 +32,71 @@ function App() {
             <div className="container-hello">
               <p>Hi everyone 👋</p>
               <h3>I´m Martin Salazar</h3>
-              <p>I´m <strong style={{ color: 'var(--principal-color)' }}>web developer</strong></p>
+              <p>I´m <strong style={{ color: 'var(--principal-color)' }}>web developer.</strong></p>
             </div>
             <div className="contact-info-principal">
-              <p><FaCalendarDays /> 10/01/2003</p>
-              <a href='tel:+56961094078'><BsTelephoneFill /> +56 9 6109 4078</a>
-              <a href="mailto:martin.salazardev@gmail.com"><TbBrandMailgun /> martin.salazardev@gmail.com</a>
-              <a href="https://goo.gl/maps/CUbzAV73B1EuMqvu7"><BiSolidMap />Santiago, Chile</a>
+
+              <a href='tel:+56961094078'><strong style={{ color: 'var(--principal-color)', marginRight: '.8vw' }}><BsTelephoneFill /></strong> +56 9 6109 4078</a>
+
+              <a href="mailto:martin.salazardev@gmail.com"><strong style={{ color: 'var(--principal-color)', marginRight: '.8vw' }}><TbBrandMailgun /></strong> martin.salazar@dev.com</a>
+              <a style={{ justifyContent: 'flex-end' }} href="https://goo.gl/maps/CUbzAV73B1EuMqvu7"><strong style={{ color: 'var(--principal-color)', marginRight: '.8vw' }}><BiSolidMap /></strong>Santiago, Chile</a>
+              <p  > <strong style={{ color: 'var(--principal-color)', marginRight: '.8vw', }}><FaCalendarDays /></strong> 10/01/2003</p>
+
+
             </div>
           </div>
         </div>
         <div className="container-description-text">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, harum odit voluptatum itaque consectetur eius molestias temporibus pariatur? Laboriosam sapiente quisquam accusamus consequatur quibusdam obcaecati unde quas, aut aliquam corporis!</p>
+          <p>I am a passionate web developer specializing in building dynamic web applications with React, as well as designing and developing custom websites using WordPress and Elementor. My focus is on delivering engaging and functional online experiences that exceed my clients' expectations. With an unwavering commitment to quality and innovation, I'm ready to take your ideas into the digital world and make them a reality.</p>
         </div>
 
         <div className="container-what-i-do">
-
+          <div className="title">
+            <p>What i do</p>
+          </div>
+          <ul>
+            <li><strong style={{ color: 'var(--principal-color)', paddingRight: '1vw' }}><TbDevicesCode /></strong>Web sites</li>
+            <li><strong style={{ color: 'var(--principal-color)', paddingRight: '1vw' }}><BsWordpress /></strong>WordPress sites</li>
+            <li><strong style={{ color: 'var(--principal-color)', paddingRight: '1vw' }}><FaReact /></strong>Internet applications</li>
+          </ul>
+        </div>
+        <div className="container-seccion-portfolio">
+          <div className="title">
+            <p>Portfolio</p>
+          </div>
+          <div className="show-cards">
+            <Card
+              image={imgCalculator}
+              category={'Aplication'}
+              title={'Calculator'}
+            />
+            <Card
+              image={imgCalculator}
+              category={'Aplication'}
+              title={'Calculator'}
+            />
+            <Card
+              image={imgCalculator}
+              category={'Aplication'}
+              title={'Calculator'}
+            />
+            <Card
+              image={imgCalculator}
+              category={'Aplication'}
+              title={'Calculator'}
+            />
+          </div>
+        </div>
+        <div className="container-get-in-touch">
+          <div className="title">
+            <p>Get in touch</p>
+          </div>
+          <form onSubmit={''} className='form-get-in-touch'>
+            <input type="text" placeholder='Name' />
+            <input type="mail" placeholder='Email' />
+            <textarea name="message" id="message" cols="30" rows="10" placeholder='Tell me about your project'></textarea>
+            <input type="submit" value={'Send'} className='btn-form' />
+          </form>
         </div>
       </div>
     </>
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// styles
+import '../styles/Card.css';
+
+const Card = ({ image, category, title }) => {
+    return (
+        <div className='card' style={{ backgroundImage: `url(${image})` }}>
+            <p>{category}</p>
+            <h3>{title}</h3>
+        </div>
+    );
+}
+
+export default Card;
